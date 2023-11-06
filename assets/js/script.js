7c8433fcc1d54abb1719284271198942bb0d48a7
Peter Sadauskas <peter.sadauskas@gmail.com>
2023-11-06 19:37:26 +1030

    Fixed forecast to start from +1 day from current date.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,14 @@ searchButton.addEventListener('click', () => {
 });
 
 function getCoordinates(city) {
-    const geocodingApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    const geocodingApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     
     fetch(geocodingApiUrl)
         .then(response => response.json())
         .then(data => {
-            if (data && data.city) {
-                const lat = data.city.coord.lat;
-                const lon = data.city.coord.lon;
+            if (data && data.coord) {
+                const lat = data.coord.lat;
+                const lon = data.coord.lon;
                 getWeatherData(lat, lon, city);
             } else {
                 alert('City not found!');
@@ -43,7 +43,6 @@ function getWeatherData(lat, lon, city) {
 function displayCurrentWeather(weatherData) {
     currentWeatherContainer.innerHTML = '';
     
-    // Assuming weatherData contains all the necessary weather details
     let temperature = document.createElement('p');
     temperature.textContent = `Temperature: ${weatherData.main.temp} °C`;
 
@@ -61,43 +60,45 @@ function displayCurrentWeather(weatherData) {
 function displayForecast(forecastData) {
     forecastContainer.innerHTML = '';
 
-    forecastData.forEach((weatherPoint, index) => {
-        if (index % 8 === 0) { // Choosing the data point of the same time every day for consistency
+    let currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+    currentDate.setHours(0, 0, 0, 0);
+
+    let filteredForecastData = forecastData.filter(weatherPoint => {
+        let forecastDate = new Date(weatherPoint.dt * 1000);
+        return forecastDate >= currentDate;
+    });
+
+    filteredForecastData.forEach((weatherPoint, index) => {
+        if (index % 8 === 0) {
             let forecastDayEl = document.createElement('div');
             forecastDayEl.className = 'forecast-day';
 
-            // Date Element
-            let date = new Date(weatherPoint.dt * 1000).toDateString(); // Convert Unix timestamp to Date
+            let date = new Date(weatherPoint.dt * 1000).toDateString();
             let dateEl = document.createElement('p');
             dateEl.textContent = date;
 
-            // Weather Icon Element
             let iconCode = weatherPoint.weather[0].icon;
             let iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
             let iconEl = document.createElement('img');
             iconEl.src = iconUrl;
             iconEl.alt = weatherPoint.weather[0].description;
 
-            // Temperature Element
             let tempEl = document.createElement('p');
             tempEl.textContent = `Temp: ${weatherPoint.main.temp} °C`;
 
-            // Wind Speed Element
             let windEl = document.createElement('p');
             windEl.textContent = `Wind: ${weatherPoint.wind.speed} km/h`;
 
-            // Humidity Element
             let humidityEl = document.createElement('p');
             humidityEl.textContent = `Humidity: ${weatherPoint.main.humidity}%`;
 
-            // Appending elements to the forecast day element
             forecastDayEl.appendChild(dateEl);
             forecastDayEl.appendChild(iconEl);
             forecastDayEl.appendChild(tempEl);
             forecastDayEl.appendChild(windEl);
             forecastDayEl.appendChild(humidityEl);
 
-            // Appending the forecast day element to the container
             forecastContainer.appendChild(forecastDayEl);
         }
     });
@@ -108,7 +109,6 @@ function updateSearchHistory(city) {
     cityEl.textContent = city;
     searchHistoryContainer.appendChild(cityEl);
 
-    // Implement local storage for search history
     let searchHistory = localStorage.getItem('searchHistory') ?
                         JSON.parse(localStorage.getItem('searchHistory')) : [];
     
@@ -118,7 +118,6 @@ function updateSearchHistory(city) {
     }
 }
 
-// Load search history on page load
 window.onload = () => {
     let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
     searchHistory.forEach(city => {
